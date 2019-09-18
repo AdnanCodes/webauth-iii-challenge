973d5cfe65715452a35df5ff889dc6d2570ccb0d
Adnan <adnan.chowdhury@outlook.com>
2019-09-18 16:24:42 -0500

    token version of same project

--- index.js
+++ index.js
@@ -2,35 +2,29 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const restricted = require("./middleware/restricted-middleware");
-const session = require("express-session");
+//const session = require("express-session");
+const jwt = require("jsonwebtoken");
+const secrets = require("./config/secrets");
 const Users = require("./users/users-model");
 
 const server = express();
 
-const sessionConfig = {
-  name: "chocochip",
-  secret: process.env.SESSION_SECRET || "keep it secret, keep it safe",
-  cookie: {
-    maxAge: 1000 * 60 * 60, // in milliseconds
-    secure: false, //true means only send cookie over https
-    httpOnly: true //True means JS has no access to the cookie
-  },
-  resave: false,
-  saveUninitialized: true // GDPR Compliance
-};
+// const sessionConfig = {
+//   name: "chocochip",
+//   secret: process.env.SESSION_SECRET || "keep it secret, keep it safe",
+//   cookie: {
+//     maxAge: 1000 * 60 * 60, // in milliseconds
+//     secure: false, //true means only send cookie over https
+//     httpOnly: true //True means JS has no access to the cookie
+//   },
+//   resave: false,
+//   saveUninitialized: true // GDPR Compliance
+// };
 
-server.use(session(sessionConfig));
+// server.use(session(sessionConfig));
 
 server.use(express.json());
-server.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true
-  })
-);
+server.use(cors());
 server.get("/", (req, res) => {
   res.send("It's alive!");
 });
@@ -55,8 +49,8 @@ server.post("/api/login", (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      req.session.user = user;
-      console.log(req.header);
+      //req.session.user = user;
+      const token = generateToken(user);
       if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
@@ -76,21 +70,33 @@ server.get("/api/users", restricted, (req, res) => {
     .catch(err => res.send(err));
 });
 
-server.get("/api/logout", (req, res) => {
-  if (req.session) {
-    req.session.destroy(error => {
-      if (error) {
-        res.status(500).json({
-          message: "You can check out anytime you like, but you can never leave"
-        });
-      } else {
-        res.status(200).json({ message: "Bye come back soon" });
-      }
-    });
-  } else {
-    res.status(200).json({ messsage: "already logged out" });
-  }
-});
+// server.get("/api/logout", (req, res) => {
+//   if (req.session) {
+//     req.session.destroy(error => {
+//       if (error) {
+//         res.status(500).json({
+//           message: "You can check out anytime you like, but you can never leave"
+//         });
+//       } else {
+//         res.status(200).json({ message: "Bye come back soon" });
+//       }
+//     });
+//   } else {
+//     res.status(200).json({ messsage: "already logged out" });
+//   }
+// });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const secret = secrets.jwtSecret;
+
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(payload, secret, options);
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
